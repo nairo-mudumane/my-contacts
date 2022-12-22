0a24167997a6c6700c3f52ead0471e40e42b559e
nairo-mudumane <nairomudumane@outlook.com>
2022-12-22 12:19:43 +0200

    firebase connection

---
+++ src/services/database.ts
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import * as firebase from "firebase-admin";
+import { ServiceAccount } from "firebase-admin";
+import serviceAccount from "./service-account.json";
+
+dotenv.config();
+
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount as ServiceAccount),
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+});
+
+export const mediaStorage = firebase.storage().bucket();
+export const firestore = firebase.firestore();
